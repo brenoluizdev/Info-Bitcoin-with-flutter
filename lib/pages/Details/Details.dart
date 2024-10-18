c6f4a67e5e87ee43c237505b3b6015b9afc667fa
brenoluizdev <brenoluizpv3@gmail.com>
2024-10-18 07:16:15 -0300

    Integrando rotas

---
+++ lib/pages/Details/Details.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
