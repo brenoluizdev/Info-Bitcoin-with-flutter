54d4d0d240e623413e5bd85f35f82c6d3cfe1029
brenoluizdev <brenoluizpv3@gmail.com>
2024-10-18 22:50:00 -0300

    iniciando input de pesquisa

--- lib/pages/HomeScreen/HomeScreen.dart
+++ lib/pages/HomeScreen/HomeScreen.dart
@@ -7,27 +7,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('InfoBitcoin', style: TextStyle(fontFamily: 'Bebas', fontWeight: FontWeight.bold, fontSize: 30),),
-        centerTitle: true,
-        foregroundColor: Colors.orangeAccent,
-        backgroundColor: Colors.black,
-        elevation: 0,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_vert),
+        appBar: AppBar(
+          title: const Text(
+            'InfoBitcoin',
+            style: TextStyle(
+                fontFamily: 'Bebas', fontWeight: FontWeight.bold, fontSize: 30),
           ),
-        ],
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/details');
-          },
-          child: const Text('Launch screen'),
+          centerTitle: true,
+          foregroundColor: Colors.orangeAccent,
+          backgroundColor: Colors.black,
+          elevation: 0,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/details');
+              },
+              icon: const Icon(Icons.more_vert),
+            ),
+          ],
         ),
-      ),
-    );
+        body: Container(
+            width: double.infinity,
+            color: Colors.black,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.search),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Pesquisar...',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )));
   }
 }
