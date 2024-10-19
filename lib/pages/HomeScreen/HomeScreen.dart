052e9d4d405d811e7925809a50a0dd5bb2d5fb62
brenoluizdev <brenoluizpv3@gmail.com>
2024-10-19 16:51:33 -0300

    comentário

--- lib/pages/HomeScreen/HomeScreen.dart
+++ lib/pages/HomeScreen/HomeScreen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, library_private_types_in_public_api
 import 'package:flutter/material.dart';
 import 'package:info_bitcoin/pages/custom/search_input.dart';
 
